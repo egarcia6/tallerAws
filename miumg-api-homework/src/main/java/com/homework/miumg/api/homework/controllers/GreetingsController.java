d569dd712ee732018575feda6ec4f014e02d0922
egarcia6 <109329411+egarcia6@users.noreply.github.com>
2022-10-15 00:47:47 -0600

    Add files via upload

---
+++ miumg-api-homework/src/main/java/com/homework/miumg/api/homework/controllers/GreetingsController.java
@@ -0,0 +1,32 @@
+package com.homework.miumg.api.homework.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * A sample greetings controller to return greeting text
+ */
+@RestController
+public class GreetingsController {
+    /**
+     *
+     * @param name the name to greet
+     * @return greeting text
+     */
+    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public String greetingText(@PathVariable String name) {
+        return "Hello " + name + "!";
+    }
+    
+    @RequestMapping(value = "/")
+    public String home(){
+        System.out.println("Log inicial");
+            return "Hola mundo";
+    }
+}
